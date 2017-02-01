bfbfbd9290c497a2a53d0cbed20be05c325e495e
KrazyDog13 <LKeane13@gmail.com>
2017-02-01 11:00:17 -0800

    Turtle 2nd Part
    My file with rectangle

--- Turtle.py
+++ Turtle.py
@@ -6,3 +6,13 @@
 
 """Import and Call the DrawRectangle(Anyturtle, l, w) function from the
 file MyFile.py"""
+#This file can be seen in my MyFile.py file and it is pasted here
+import turtle
+LogansTurtle = turtle.Turtle()
+def DrawRectangle(Anyturtle, l, w):
+    for x in range(4):
+        Anyturtle.forward(12)
+        Anyturtle.right(90)
+        Anyturtle.forward(w)
+DrawRectangle(LogansTurtle, 4, 4)
+print(DrawRectangle(Anyturtle, l, w))
