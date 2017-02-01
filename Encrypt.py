568a6a7b9c7df67c363bf29031aeab737034d704
KrazyDog13 <LKeane13@gmail.com>
2017-02-01 11:46:53 -0800

    Encrypt First Part

--- Encrypt.py
+++ Encrypt.py
@@ -4,10 +4,28 @@
 
 Print the save the result as the variable = NoVowels
 """"
+def NoVowels(AnyString):
+    vowels = ('a', 'e', 'i', 'o', 'u' , 'A', 'E', 'I', 'O',
+    'U')
+    newString = ""
+    for ch in AnyString:
+        if ord(ch) is 97 or ord(ch) is 65:
+            newString = newString + ""
+        elif ord(ch) is 101 or ord(ch) is 69:
+            newString = newString + ""
+        elif ord(ch) is 105 or ord(ch) is 73:
+            newString = newString + ""
+        elif ord(ch) is 111 or ord(ch) is 79:
+            newString = newString + ""
+        elif ord(ch) is 117 or ord(ch) is 85:
+            newString = newString + ""
 
 
+        else:
+            newString = newString + ch
+        return newString
+String = """Computer Science Makes the World go round but it doesn't make the world round itself!"""
 
-
-
-
+code = final(NoVowels)
+print(code)
 """Write an encryption code that you make up and run it for the variable NoVowels"""
